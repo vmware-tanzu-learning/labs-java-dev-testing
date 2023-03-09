e3d7ce215f6dea7ac74381c47e99b1be5c1c1308
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2023-03-09 17:23:04 +1100

    Setup extension installation.

--- workshops/lab-example-workshop-1/workshop/setup.d/01-install-extensions.sh
+++ workshops/lab-example-workshop-1/workshop/setup.d/01-install-extensions.sh
@@ -3,8 +3,8 @@
 set -x
 set -eo pipefail
 
-# This can take up to 2 minutes to install directly since open-vsx.org is so slow.
-# The set of packages which it installs should be:
+# This can take up to 2 minutes, or even worse, to install directly since open-vsx.org
+# is so slow. The set of packages which it installs should be:
 #
 # redhat.java-1.15.0-linux-x64
 # vscjava.vscode-java-debug-0.49.0-universal
